fc58421c377fff022080a8817cdeeac22e3f7624
hamzahasa067 <hamzahasa067@gmail.com>
2022-12-06 10:47:59 +0200

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response, NextFunction} from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT;
+
+// new commnet un saved
+app.listen(port, () => {
+    console.log("Server is running .....");   
+})
